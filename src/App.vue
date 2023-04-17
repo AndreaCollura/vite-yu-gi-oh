1be16d703545bf6b88ae60e9ed73ac46934b2adf
AndreaCollura <andrea.collur@gmail.com>
2023-04-17 16:01:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+import { store } from './data/store';
+import axios from 'axios';
+
+  export default {
+    name: 'App',
+    components: {
+
+    },
+    data(){
+      return{
+        store
+      }
+    },
+    methods: {
+      getCards() {
+      const url = store.baseUrl + store.endpoint;
+      axios.get(url).then((res) => {
+        store.cardList = res.data.data;
+        console.log(res.data.data);
+      });
+    }
+
+    },
+    mounted(){
+      this.getCards()
+
+    }
+
+    
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
